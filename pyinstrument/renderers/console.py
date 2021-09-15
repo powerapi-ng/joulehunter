3dad6fd42fa4b56cda56fa58578f995f7cc1a6ed
akaminetzkyp <akaminetzkyp@gmail.com>
2021-09-15 15:44:00 +0200

    Split line that was too long

--- pyinstrument/renderers/console.py
+++ pyinstrument/renderers/console.py
@@ -50,7 +50,8 @@ class ConsoleRenderer(Renderer):
             r"",
             r"  _     ._   __/__   _ _  _  _ _/_  ",
             r" /_//_/// /_\ / //_// / //_'/ //    ",
-            r"/   _/        {:>20}".format("v" + pyinstrument.__version__ + " (netzky fork)"),
+            r"/   _/        {:>20}".format("v" + pyinstrument.__version__
+                                           + " (netzky fork)"),
         ]
 
         lines[1] += " Recorded: {:<9}".format(
