10dcc40f43534ad14e23080ca36d8d394f34b5d8
mbelgaid <mohammed-chakib.belgaid@inria.fr>
2022-11-02 17:59:09 +0100

    update : fix package bug

--- joulehunter/__init__.py
+++ joulehunter/__init__.py
@@ -2,7 +2,7 @@ import warnings
 
 from joulehunter.profiler import Profiler
 
-__version__ = "v1.0.1"
+__version__ = "v1.0.2"
 
 # enable deprecation warnings
 warnings.filterwarnings("once", ".*", DeprecationWarning, r"joulehunter\..*")
